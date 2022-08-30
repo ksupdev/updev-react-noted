2ff4e2feb622edc5994794d29ef63247a8acb786
karoon sillapapan <karoons.updev@gmail.com>
2022-08-30 21:53:28 +0700

    done: ep8 State and Hook

--- workshops/hello-react/src/App.js
+++ workshops/hello-react/src/App.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default () => {
-  const var1 = 0;
+  let var1 = 0;
   const var2 = "hello";
   const var3 = true;
   const var4 = false;
 
+  // Hook
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Karoon {var1}</h1>
+      <h1>Var1 {var1}</h1>
       <h1>Karoon {var2}</h1>
       <h1>Karoon {var3 && "yes"}</h1>
       <h1>Karoon {var4 && "yes"}</h1>
       {var3 && <h1>YES</h1>}
       {var4 ? <h1>YES</h1> : <h1>NO</h1>}
+      <h1>Count : {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          console.log(count);
+        }}
+      >
+        Add (Count value)
+      </button>
     </div>
   );
 };
